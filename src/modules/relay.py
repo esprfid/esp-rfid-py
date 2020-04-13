e76898579a58fc04925ad1226fc89584d1bc57c6
Honza Pobořil <honza@poboril.cz>
2020-04-13 03:28:31 +0200

    Rewrote relay module to async, configurable and support for multiple relays.

--- src/modules/relay.py
+++ src/modules/relay.py
@@ -1,20 +1,34 @@
 from machine import Pin
-#import uasyncio as asyncio
+import uasyncio as asyncio
+loop = asyncio.get_event_loop()
 import utime
 import logging
 log = logging.getLogger("relay")
 import events
 
-RELAY_OPEN_DURATION = 1
-# TODO: Use config and support multiple relays
 
-pin = Pin(13, Pin.OUT)
+# Load config and initiate pins
+import config
+relays = config.modules['relay']['relays']
+for r in relays:
+	r.update({
+		'pin_obj': Pin(r['pin'], Pin.OUT)
+	})
+
 
 @events.on('card.card_validated')
 def on_card_validated(card):
-	log.info("Opening relay for %d seconds", RELAY_OPEN_DURATION)
-	pin.on()
-	utime.sleep(RELAY_OPEN_DURATION)
-	pin.off()
-	# TODO: Use fast_io to turn off relay
-	#asyncio.get_event_loop().call_later(RELAY_OPEN_DURATION, lambda: pin.off())
+	for config in relays:
+		loop.create_task(relay_task(config, card))
+	# TODO: Solve delay before relay_task is actally started.
+
+
+async def relay_task(config, card):
+	if 'open_after' in config:
+		log.info("Will open relay on pin %d in %d seconds", config['pin'], config['open_after'])
+		await asyncio.sleep(config['open_after'])
+
+	log.info("Opening relay on pin %d for %d seconds", config['pin'], config['close_after'])
+	config['pin_obj'].on()
+	await asyncio.sleep(config['close_after'])
+	config['pin_obj'].off()
